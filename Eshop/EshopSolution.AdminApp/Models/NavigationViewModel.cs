f13cf98797edb65829f0be3943f5bc4fff10c748
nguyễn thanh hiền <thanhhienm4@gmail.com>
2020-09-22 16:12:52 +0700

    update

---
+++ Eshop/EshopSolution.AdminApp/Models/NavigationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EshopSolution.ViewModel.System.Languages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EshopSolution.AdminApp.Models
+{
+    public class NavigationViewModel
+    {
+        public List<LanguageViewModel> Languages { get; set; }
+        public string CurrentLanguageId { get; set; }
+    }
+}
